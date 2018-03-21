f9ad89969749d5f32f83b33d0d60212b873836c9
michaelbaud <michael.baudoin.7@gmail.com>
2018-03-21 12:43:38 +0100

    refonte michael

--- javascript.js
+++ javascript.js
@@ -1,19 +1,20 @@
-//bandeau hoover
+$(document).ready(function() {
 
-function imagedyn(){
-  result = document.getElementById("lienmagic")
-  result.style.visibility = "visible";
-}
-function imagedyn2(){
-  result = document.getElementById("lienmagic")
-  result.style.visibility = "hidden";
-}
-//
-//carrousel
-var myIndex = 0;
-carousel();
+  //bandeau hoover
+  function imagedyn(){
+    result = document.getElementById("lienmagic")
+    result.style.visibility = "visible";
+  }
+  function imagedyn2(){
+    result = document.getElementById("lienmagic")
+    result.style.visibility = "hidden";
+  }
+  
+  //carrousel
+  var myIndex = 0;
+  carousel();
 
-function carousel() {
+  function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
@@ -23,14 +24,10 @@ function carousel() {
     if (myIndex > x.length) {myIndex = 1}
     x[myIndex-1].style.display = "block";
     setTimeout(carousel, 4200); // Change image every 2 seconds
-}
-//fin du carrousel
+  }
+  //fin du carrousel
 
-
-// début onClickLeft et onClickRight de la partie simplon-quoi
-
-$(document).ready(function() {
-
+  // début onClickLeft et onClickRight de la partie simplon-quoi
   var onClickLeft = document.querySelector(".onClickLeft");
   var onClickRight = document.querySelector(".onClickRight");
 
@@ -52,89 +49,125 @@ $(document).ready(function() {
   crossRight.onclick = function() {
     onClickRight.classList.toggle("onClickRight_open");
   }
+  // fin onClickLeft et onClickRight de la partie simplon-quoi
+
+  // début hover des image de témoins
+  $(".photoTemoin1").mouseover(function(){
+    $(".text-image1").css("visibility", "visible");
+  });
+  $(".photoTemoin1").mouseout(function(){
+    $(".text-image1").css("visibility", "hidden");
+  });
+
+  $(".photoTemoin2").mouseover(function(){
+    $(".text-image2").css("visibility", "visible");
+  });
+  $(".photoTemoin2").mouseout(function(){
+    $(".text-image2").css("visibility", "hidden");
+  });
+
+  $(".photoTemoin3").mouseover(function(){
+    $(".text-image3").css("visibility", "visible");
+  });
+  $(".photoTemoin3").mouseout(function(){
+    $(".text-image3").css("visibility", "hidden");
+  });
+
+  $(".photoTemoin4").mouseover(function(){
+    $(".text-image4").css("visibility", "visible");
+  });
+  $(".photoTemoin4").mouseout(function(){
+    $(".text-image4").css("visibility", "hidden");
+  });
+
+  $(".photoTemoin5").mouseover(function(){
+    $(".text-image5").css("visibility", "visible");
+  });
+  $(".photoTemoin5").mouseout(function(){
+    $(".text-image5").css("visibility", "hidden");
+  });
+  // fin hover des image de témoins
+
+  // début carte
+  var villes = document.getElementsByClassName('fabrique');
+  //var afterClick = document.getElementById('ville1');
+  var text = {
+  paris:'Ile-de-France: 9 fabriques',
+  est:'Grand-Est : 2 fabriques',
+  nEst:'Hauts-de-France: 2 fabriques',
+  occ:"Occitanie: 10 fabriques",
+  auvergne:"Auvergne-Rhône-Alpes: 4 fabriques",
+  paca:"PACA: 2 fabriques",
+  aquitaine:"Nouvelle-Aquitaine: 3 fabriques",
+  bretagne:"Bretagne: 3 fabriques"
+  }
+
+  var count = 1;
+
+  Array.prototype.forEach.call(villes,function(ville){
+  var id = ville.getAttribute("id");
+  ville.onclick = function(){
+  if(count==0){
+
+    ville.style.backgroundColor="red";
+    ville.style.opacity="1";
+    ville.style.borderRadius="50%";
+    ville.style.height="15px";
+    ville.style.width="15px";
+    ville.style.padding="0";
+    ville.style.border="none";
+    ville.innerText = null;
+
+    count=1;
+  }
+  else{
+   ville.style.backgroundColor="white";
+   ville.style.opacity="0.7";
+   ville.style.display="flex";
+   ville.style.flexWrap="wrap";
+   ville.style.height="auto";
+   ville.style.width="100px";
+   ville.style.borderRadius="3px";
+   ville.style.border="2px solid black";
+   ville.style.textAlign="center";
+   ville.style.verticalAlign="center";
+  window.setTimeout(afficheText,550)
+  function afficheText(){
+    switch (id){
+       case 'Paris':
+       ville.innerText = text.paris;
+       break;
+       case 'Est':
+       ville.innerText = text.est;
+       break;
+       case 'Nord-Est':
+       ville.innerText = text.nEst;
+       break;
+       case 'Occitanie':
+       ville.innerText = text.occ;
+       break;
+       case 'Auvergne':
+       ville.innerText = text.auvergne;
+       break;
+       case 'paca':
+       ville.innerText = text.paca;
+       break;
+       case 'aquitaine':
+       ville.innerText = text.aquitaine;
+       break;
+       case 'bretagne':
+       ville.innerText = text.bretagne;
+     };
+   }
+
+   ville.style.padding="10px 15px 10px 15px";
+   ville.style.webkitTransition="0.5s";
+   ville.style.textAlign="center";
+   ville.style.fontFamily="'Lato";
+   count=0;
+  }}})
+  // fin carte
 
 });
-// fin onClickLeft et onClickRight de la partie simplon-quoi
-
-//Carte
-
-var villes = document.getElementsByClassName('fabrique');
-//var afterClick = document.getElementById('ville1');
-var text = {
-paris:'Ile-de-France: 9 fabriques',
-est:'Grand-Est : 2 fabriques',
-nEst:'Hauts-de-France: 2 fabriques',
-occ:"Occitanie: 10 fabriques",
-auvergne:"Auvergne-Rhône-Alpes: 4 fabriques",
-paca:"PACA: 2 fabriques",
-aquitaine:"Nouvelle-Aquitaine: 3 fabriques",
-bretagne:"Bretagne: 3 fabriques"
-
-}
 
 
-var count = 1;
-
-Array.prototype.forEach.call(villes,function(ville){
-var id = ville.getAttribute("id");
-ville.onclick = function(){
-if(count==0){
-
-  ville.style.backgroundColor="red";
-  ville.style.opacity="1";
-  ville.style.borderRadius="50%";
-  ville.style.height="15px";
-  ville.style.width="15px";
-  ville.style.padding="0";
-  ville.style.border="none";
-  ville.innerText = null;
-
-  count=1;
-}
-else{
- ville.style.backgroundColor="white";
- ville.style.opacity="0.7";
- ville.style.display="flex";
- ville.style.flexWrap="wrap";
- ville.style.height="auto";
- ville.style.width="100px";
- ville.style.borderRadius="3px";
- ville.style.border="2px solid black";
- ville.style.textAlign="center";
- ville.style.verticalAlign="center";
-window.setTimeout(afficheText,550)
-function afficheText(){
-  switch (id){
-     case 'Paris':
-     ville.innerText = text.paris;
-     break;
-     case 'Est':
-     ville.innerText = text.est;
-     break;
-     case 'Nord-Est':
-     ville.innerText = text.nEst;
-     break;
-     case 'Occitanie':
-     ville.innerText = text.occ;
-     break;
-     case 'Auvergne':
-     ville.innerText = text.auvergne;
-     break;
-     case 'paca':
-     ville.innerText = text.paca;
-     break;
-     case 'aquitaine':
-     ville.innerText = text.aquitaine;
-     break;
-     case 'bretagne':
-     ville.innerText = text.bretagne;
-   };
- }
-
-
- ville.style.padding="10px 15px 10px 15px";
- ville.style.webkitTransition="0.5s";
- ville.style.textAlign="center";
- ville.style.fontFamily="'Lato";
- count=0;
-}}})
